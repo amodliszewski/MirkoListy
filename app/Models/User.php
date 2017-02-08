0e31a543288df83bcb78039271777edd9c0dbf51
Elon Than <wolinskip@hws.pl>
2017-02-08 14:03:59 +0100

    rzutowanie wartości, closes #2

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,17 @@ use WykoCommon\Models\User as Base;
 
 class User extends Base
 {
+    protected $casts = [
+        'id' => 'integer',
+        'rights' => 'integer',
+        'color' => 'integer',
+        'sex' => 'integer',
+        'call_optout' => 'integer',
+        'created_count' => 'integer',
+        'joined_count' => 'integer',
+        'called_count' => 'integer'
+    ];
+
     public function spamlists()
     {
         return $this->belongsToMany('App\Models\Spamlist', 'user_spamlists');
