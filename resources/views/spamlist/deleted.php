7517b5532364dcca46f3f8e3947aa6d73b66891b
Elon Than <wolinskip@hws.pl>
2017-03-01 16:24:18 +0100

    link do logów usuniętej listy

--- resources/views/spamlist/deleted.php
+++ resources/views/spamlist/deleted.php
@@ -13,10 +13,11 @@
             <div class="col-lg-12">
                 <div class="jumbotron">
                     <p>Lista została usunięta <strong><?php echo $item->deleted_at; ?></strong> przez <strong><?php echo $app->make('App\Services\TemplateService')->getUserProfileUrl($item->deletedBy); ?></strong></p>
+
+                    <a href="<?php echo route('spamlistLogsUrl', ['uid' => $item->uid]); ?>">zobacz wydarzenia z tej listy</a>
                 </div>
             </div>
         </div>
 
 <?php
     require dirname(__FILE__) . '/../footer.php';
-?>
