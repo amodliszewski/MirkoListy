494aaaa27b4950c2b3f8f8225407ee45a11903cf
Piotr Woliński <wolinskip@hws.pl>
2017-07-21 22:31:09 +0200

    aktualizacja profili userów wybranej listy

---
+++ app/Console/Commands/RefreshSpamlistUsers.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use WykoCommon\Services\WykopAPI;
+use App\Models\Spamlist;
+use App\Models\UserSpamlist;
+
+class RefreshSpamlistUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refresh:spamlist {uid}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refresh spamlist users';
+
+    /**
+     *
+     * @var \WykoCommon\Services\WykopAPI
+     */
+    private $wykopAPI = null;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(WykopAPI $wykopAPI) {
+        parent::__construct();
+
+        $this->wykopAPI = $wykopAPI;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        $spamlist = Spamlist::where('uid', $this->argument('uid'))
+                ->first();
+
+        if ($spamlist === null) {
+            return;
+        }
+
+        $items = UserSpamlist::where('spamlist_id', $spamlist->id)
+                ->get();
+
+        if ($items === null) {
+            return;
+        }
+
+        foreach ($items as $key => $item) {
+            $result = $this->wykopAPI->doRequest('profile/index/' . $item->user->nick . ',userkey,' . $item->user_key);
+
+            $item->user->nick = $result['login'];
+            $item->user->avatar_url = $result['avatar_med'];
+            $item->user->color = $result['author_group'];
+
+            if ($result['sex'] === 'male') {
+                $item->user->sex = 1;
+            } else if ($result['sex'] === 'female') {
+                $item->user->sex = 2;
+            } else {
+                $item->user->sex = 0;
+            }
+
+            $item->user->updated_at = time();
+
+            $item->user->save();
+
+            $this->info('Zaktualizowano ' . ($key + 1 ) . ' / ' . $items->count());
+
+            if ($key % 9 === 0) {
+                sleep(5);
+            }
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         'App\Console\Commands\Scheduled',
+        'App\Console\Commands\RefreshSpamlistUsers',
         'WykoCommon\Console\Commands\ProcessQueue'
     ];
 
