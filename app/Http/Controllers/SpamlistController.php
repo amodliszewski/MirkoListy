1b68bdb7f023c8651924afbd78eba55edbad546c
Elon Than <wolinskip@hws.pl>
2018-11-10 18:33:21 +0100

    poprawka sprawdzania kategorii

--- app/Http/Controllers/SpamlistController.php
+++ app/Http/Controllers/SpamlistController.php
@@ -652,8 +652,8 @@ class SpamlistController extends Controller
             return response(view('errors/404'), 404);
         }
 
-		if ($entity->category() !== null && $entity->category()->limited) {
-            $request->session()->flash('flashError', 'Usuwanie użytkowników z list z kategorii ' . $entity->category()->name . ' zostało zablokowane');
+		if ($newRights === -1 && $entity->category !== null && $entity->category->limited) {
+            $request->session()->flash('flashError', 'Usuwanie użytkowników z list z kategorii ' . $entity->category->name . ' zostało zablokowane');
 
             return redirect()->back();
 		}
